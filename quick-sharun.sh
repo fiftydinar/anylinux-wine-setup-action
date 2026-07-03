a28385cece00d6b3d11fa29fd818e1234286a719
fiftydinar <srbaizoki4@tuta.io>
2026-07-03 21:42:45 +0200

    quick-sharun: mkdir -p full WINEPREFIX, cp into WINEPREFIX, wine WINEPREFIX/rufus.exe

--- quick-sharun.sh
+++ quick-sharun.sh
@@ -2990,12 +2990,12 @@ if [ -n "$WINE_MAIN_BIN" ]; then
 	cat <<EOF > "$DST_BIN_DIR"/"$MAIN_BIN"
 #!/bin/sh
 WINEPREFIX="\${XDG_DATA_HOME}/anylinux-wine/${WINE_MAIN_BIN%.exe}"
-mkdir -p "\${WINEPREFIX%/*}"
+mkdir -p "\${WINEPREFIX}"
 if [ ! -d "\${WINEPREFIX}" ]; then
     wineboot
 fi
-cp -rn "\${APPDIR}/share/${WINE_MAIN_BIN%.exe}" "\${WINEPREFIX}/${WINE_MAIN_BIN%.exe}"
-wine "\${WINEPREFIX}/${WINE_MAIN_BIN%.exe}/${WINE_MAIN_BIN}" "\$@"
+cp -rn "\${APPDIR}/share/${WINE_MAIN_BIN%.exe}" "\${WINEPREFIX}"
+wine "\${WINEPREFIX}/${WINE_MAIN_BIN}" "\$@"
 EOF
 	chmod +x "$DST_BIN_DIR"/"$MAIN_BIN"
 fi
