2d8864297d5c63116ed3cf2a3ddb80fda64ed8bb
fiftydinar <srbaizoki4@tuta.io>
2026-07-03 22:14:30 +0200

    quick-sharun: always mkdir -p WINEPREFIX, check system.reg for prefix init
    Fixes cp -rn dir/. failing when dest doesn't exist, and ensures
    rufus.exe is always copied even if wineboot fails (no display).

--- quick-sharun.sh
+++ quick-sharun.sh
@@ -2990,11 +2990,11 @@ if [ -n "$WINE_MAIN_BIN" ]; then
 	cat <<EOF > "$DST_BIN_DIR"/"$MAIN_BIN"
 #!/bin/sh
 WINEPREFIX="\${XDG_DATA_HOME}/anylinux-wine/${WINE_MAIN_BIN%.exe}"
-mkdir -p "\${WINEPREFIX%/*}"
-if [ ! -d "\${WINEPREFIX}" ]; then
+mkdir -p "\${WINEPREFIX}"
+cp -rn "\${APPDIR}/share/${WINE_MAIN_BIN%.exe}/." "\${WINEPREFIX}"
+if [ ! -f "\${WINEPREFIX}/system.reg" ]; then
     wineboot
 fi
-cp -rn "\${APPDIR}/share/${WINE_MAIN_BIN%.exe}/." "\${WINEPREFIX}"
 wine "\${WINEPREFIX}/${WINE_MAIN_BIN}" "\$@"
 EOF
 	chmod +x "$DST_BIN_DIR"/"$MAIN_BIN"
